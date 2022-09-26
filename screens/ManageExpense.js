e91f251d97722c86ee1ed998877c069ca1c84482
David Lindner Jr <davidlindnerjr@Davids-Air.attlocal.net>
2022-09-26 10:52:07 -0700

    Initial commit

---
+++ screens/ManageExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ManageExpense = () => {
+  return (
+    <View>
+        <Text>
+            ManageExpenses
+        </Text>
+    </View>
+  )
+}
+
+export default ManageExpense
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RecentExpenses = () => {
+  return (
+    <View>
+        <Text>
+            RecentExpenses
+        </Text>
+    </View>
+  )
+}
+
+export default RecentExpenses
